9b74f6a582b2807a6ec875187d6dc18a0f2ada62
fsouzadi1995 <fsouzadi1995@gmail.com>
2021-05-15 23:27:29 -0300

    chore: add new endpoint

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,8 @@ router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-// router.get('/1', function (req, res, next) {
-//   res.send({ name: 'Fabián', age: '25' });
-// });
+router.get('/1', function (req, res, next) {
+  res.send({ name: 'Fabián', age: '25' });
+});
 
 module.exports = router;
